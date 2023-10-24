3e1087db925d22dbcb6a19dcf60d4b71ffa8c4c8
Slav <slav.1994@icloud.com>
2023-10-24 10:26:19 +0300

    new

--- src/js/services/_services.js
+++ src/js/services/_services.js
@@ -10,21 +10,7 @@ const postData = async (url, data) => {
   return await res.json();
 };
 
-// async function getResource(url) {
-//   let res = await fetch(url);
 
-//   if (!res.ok) {
-//     throw new Error(`Could not fetch ${url}, status: ${res.status}`);
-//   }
-//   try {
-//     const data = await res.json();
-//     return data;
-//   }
-//   cache(error) {
-//     console.log(error);
-//   }
-//   return await res.json();
-// }
 async function getResource(url) {
   let res = await fetch(url);
 
